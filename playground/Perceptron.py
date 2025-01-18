1f0de34845cefbe370c0d113b5c66c070d0d4e11
temmie0232 <temmie0232@gmail.com>
2025-01-18 13:52:08 +0900

    a

---
+++ playground/Perceptron.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Perceptron(object)
+    
+    def __init__(self,eta=0.01,n_iter=50,random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self,X,y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01,size=1+X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi,target in zip(X,y):
+                update = self.eta * (target - self.predict(xi))
